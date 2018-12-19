7b72b177710608058d15588df71b59e9904ec28d
Artsiom Zhalkouski <azhalkouski@gmail.com>
2018-12-19 15:40:07 +0300

    Fix index.js and test script

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import compose from "recompose/compose";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import AuthFormStateContainer from "./AuthFormStateContainer.jsx";
+import AuthFormContainer from "./AuthFormContainer.jsx";
+import SignUpFormView from "./SignUpFormView.jsx";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import submit from "./submit";
+import validate from "./validate";
+
+const SignUpForm = compose(
+  AuthFormStateContainer,
+  AuthFormContainer
+)(SignUpFormView);
+
+ReactDOM.render(
+  <SignUpForm title="Sign Up" submit={submit} validate={validate} />,
+  document.getElementById("root")
+);
